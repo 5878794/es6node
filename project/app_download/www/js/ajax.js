7d7c69ee6ea0f56974b1c6c3f89134f35d552b1c
5878794@qq.com <xf771026aa>
2017-06-27 10:12:30 +0800

    add app_download  set ver support

--- project/app_download/www/js/ajax.js
+++ project/app_download/www/js/ajax.js
@@ -41,7 +41,7 @@ var getList = function(){
         var data = rs;
         for(var i=0,l=data.length;i<l;i++){
             var _data = data[i],
-                div = $("<div class='div' style='border-bottom:1px solid #ccc; padding: 20px 0' id='"+_data.id+"' ver='"+_data.ver+"'><span style='padding-left: 30px;'>"+_data.name+" </span>   版本:<input style='width: 60px;' type='text' value='"+_data.ver+"'/><span class='download_app' style='padding: 0 10px;'>下载</span></div>");
+                div = $("<div class='div' style='border-bottom:1px solid #ccc; padding: 20px 0' id='"+_data.id+"' ver='"+_data.ver+"'><span style='padding-left: 30px;'>"+_data.name+" </span>   版本:<input style='width: 60px;' type='text' value='"+_data.ver+"'/><span class='download_app' style='padding: 0 10px; background: #000; color: #fff;'>下载</span></div>");
 
             div.find(".download_app").click(function(){
                 var id = $(this).parent().attr("id"),
