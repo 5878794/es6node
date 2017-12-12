8717c97bdeb5fd300aad7d9d83292162033fcc77
5878794@qq.com <xf771026aa>
2017-12-12 13:50:11 +0800

    add p3 p4 animate

--- project/viaila/www/js/p3.js
+++ project/viaila/www/js/p3.js
@@ -6,3 +6,34 @@
 
 var ProductDian = [0,950,1720,2600];
 var ProductClass = ['','','',''];
+
+
+
+var animate = [0,950,1720,2600];
+var animateType = [
+	[
+		{type:'up',val:100,time:500,detail:0},
+		{type:'',val:'',time:500,detail:400}
+	],
+	[
+		{type:'',val:'',time:1000,detail:0},
+		{type:'up',val:100,time:500,detail:0},
+		{type:'down',val:100,time:500,detail:400},
+		{type:'down',val:100,time:500,detail:900}
+	],
+	[
+		{type:'',val:100,time:500,detail:0},
+		{type:'left',val:100,time:500,detail:400},
+		{type:'right',val:100,time:500,detail:900},
+		{type:'left',val:100,time:500,detail:1400},
+		{type:'right',val:100,time:500,detail:1900},
+		{type:'left',val:100,time:500,detail:2400},
+		{type:'right',val:100,time:500,detail:2900},
+		{type:'down',val:100,time:500,detail:3400}
+	],
+	[
+		{type:'',val:100,time:500,detail:0},
+		{type:'up',val:100,time:500,detail:400},
+		{type:'down',val:100,time:500,detail:900}
+	]
+];
--- project/viaila/www/js/p4.js
+++ project/viaila/www/js/p4.js
@@ -6,3 +6,45 @@
 
 var ProductDian = [0,940,1900,2700,3500,4500];
 var ProductClass = ['','','','','',''];
+
+
+
+var animate = [0,940,1900,2700,3500,4500];
+var animateType = [
+	[
+		{type:'up',val:100,time:500,detail:0},
+		{type:'',val:'',time:500,detail:400},
+		{type:'down',val:100,time:500,detail:400}
+	],
+	[
+		{type:'',val:'',time:1000,detail:0},
+		{type:'up',val:100,time:500,detail:0},
+		{type:'down',val:100,time:500,detail:700}
+	],
+	[
+		{type:'up',val:100,time:500,detail:0},
+		{type:'',val:'',time:500,detail:400},
+		{type:'right',val:100,time:500,detail:900},
+		{type:'left',val:100,time:500,detail:1400}
+	],
+	[
+		{type:'',val:100,time:500,detail:0},
+		{type:'up',val:100,time:500,detail:400},
+		{type:'down',val:100,time:500,detail:900},
+		{type:'down',val:100,time:500,detail:900},
+		{type:'down',val:100,time:500,detail:900}
+	],
+	[
+		{type:'down',val:100,time:500,detail:0},
+		{type:'',val:'',time:500,detail:400},
+		{type:'left',val:100,time:500,detail:900},
+		{type:'right',val:100,time:500,detail:900},
+		{type:'down',val:100,time:500,detail:900}
+	],
+	[
+		{type:'',val:100,time:500,detail:0},
+		{type:'up',val:100,time:500,detail:400},
+		{type:'left',val:100,time:500,detail:900},
+		{type:'right',val:100,time:500,detail:900}
+	]
+];
