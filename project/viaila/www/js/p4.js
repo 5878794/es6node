b58d8a1fdc0341e8ef6ded739d74975de3b48564
5878794@qq.com <xf771026aa>
2017-12-25 10:28:01 +0800

    mdf p4  img ===

--- project/viaila/www/js/p4.js
+++ project/viaila/www/js/p4.js
@@ -4,12 +4,12 @@
 
 
 
-var ProductDian = [0,940,1900,2700,3500,4500];
-var ProductClass = ['','','','','',''];
+var ProductDian = [0,940,1900,2700,3500,4500,5090];
+var ProductClass = ['','','','','','',''];
 
 
 
-var animate = [0,940,1900,2700,3500,4500];
+var animate = [0,940,1900,2700,3500,4500,5090];
 var animateType = [
 	[
 		{type:'up',val:100,time:500,detail:0},
@@ -35,16 +35,20 @@ var animateType = [
 		{type:'down',val:100,time:500,detail:900}
 	],
 	[
-		{type:'down',val:100,time:500,detail:0},
-		{type:'',val:'',time:500,detail:400},
-		{type:'left',val:100,time:500,detail:900},
-		{type:'right',val:100,time:500,detail:900},
-		{type:'down',val:100,time:500,detail:900}
+		{type:'',val:100,time:500,detail:0},
+		{type:'down',val:100,time:500,detail:400},
+		{type:'',val:'',time:500,detail:800},
+		{type:'left',val:100,time:500,detail:1300},
+		{type:'right',val:100,time:500,detail:1300},
+		{type:'down',val:100,time:500,detail:1300}
 	],
 	[
 		{type:'',val:100,time:500,detail:0},
 		{type:'up',val:100,time:500,detail:400},
 		{type:'left',val:100,time:500,detail:900},
 		{type:'right',val:100,time:500,detail:900}
+	],
+	[
+		{type:'',val:100,time:500,detail:0},
 	]
 ];
